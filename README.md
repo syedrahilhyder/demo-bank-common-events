f92146255953328f18d4056993b4e27f2fea70c0
Syed Hyder <syedrahilhyder@gmail.com>
2026-01-03 07:39:37 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# demo-bank-common-events
+Shared Kafka event contracts
---
+++ src/main/java/com/demo/bank/events/EventTopics.java
@@ -0,0 +1,4 @@
+package com.demo.bank.events;
+public final class EventTopics {
+  public static final String PAYMENTS_EVENTS = "demo-bank.payments.events";
+}
